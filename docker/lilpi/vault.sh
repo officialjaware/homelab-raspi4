e78f14c788c23570da46af44dc7a39278bc436a8
jitinaware <60826351+jitinaware@users.noreply.github.com>
2022-07-03 01:18:58 -0400

    updates

--- docker/lilpi/vault.sh
+++ docker/lilpi/vault.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-curl https://releases.hashicorp.com/vault/1.10.4/vault_1.10.4_linux_arm.zip -o vault.zip
+set -e
+
+curl -sS https://releases.hashicorp.com/vault/1.10.4/vault_1.10.4_linux_arm.zip -o vault.zip
 
 sudo unzip vault.zip -d /usr/local/bin/
 
-mkdir -p /etc/vault.d/file
+sudo mkdir -p /etc/vault.d/file
 
-cp -a /vault/. /etc/vault.d/
+sudo cp -a ./vault/. /etc/vault.d/
 
 docker-compose up -d
